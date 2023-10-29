e9b0f854a0c9d2614a45ba6a3345d657040f4f05
M0H1K4N <nejer911@gmail.com>
2023-10-29 20:33:41 +0400

    using rernary operators

--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -9,11 +9,11 @@ export default function Player({ name, symbol }) {
   }
 
   let playerName = <span className="player-name">{name}</span>;
-  let btnCaption = 'Edit';
+  // let btnCaption = 'Edit';
 
   if (isEditing) {
-    playerName = <input type="text" required />;
-    btnCaption = 'Save'
+    playerName = <input type="text" required value={name} />;
+    // btnCaption = 'Save'
   }
 
   return (
@@ -22,7 +22,7 @@ export default function Player({ name, symbol }) {
         {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}> {btnCaption} </button>
+      <button onClick={handleEditClick}> {isEditing ? 'Save' : 'Edit'} </button>
     </li>
   );
 }
